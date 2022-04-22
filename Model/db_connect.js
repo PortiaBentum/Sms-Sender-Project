00896d143d8aba5fb17b0385bb9d72ae4241aeb0
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 13:05:37 +0000

    make it better

--- Model/db_connect.js
+++ Model/db_connect.js
@@ -1,12 +1,22 @@
 const Pool = require("pg").Pool;
 
+// For testing locally
 
-const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'octosenda'
-});
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'octosenda'
+// });
+
+// Using the heroku App
+
+const pool = new Pool({ // create connection to database
+    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+    ssl: {
+      rejectUnauthorized: false // don't check for SSL cert
+    }
+  });
 
 module.exports = pool;
